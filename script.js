84d1e0be7cbdda75ab67e200f0e7c9969434efb5
Andrés Ladino <122582963+ladinofel@users.noreply.github.com>
2023-04-18 10:10:29 -0500

    Modify the createCanvas function to allow for user input and add a modal to interact with user

--- script.js
+++ script.js
@@ -1,95 +1,51 @@
 //Initialize variables
 let tone = '#000000';
 let multi_interval;
+let user_value;
+
+//This function asks the user for a value and calls the createCanvas and paint functions.
+function initialize(){
+  const modal = document.querySelector('dialog');
+  modal.showModal();
+  const user_input = document.querySelector('.enter-btn');
+  user_input.addEventListener('click', () => {
+    const input_content = document.querySelector('input');
+    if(input_content.value === ''){
+      alert('You must enter a value')}
+      else {
+    user_value = input_content.value;
+    createCanvas(user_value);
+    modal.close();
+    paint();
+      }
+  })
+}
+
+initialize();
 
 //This function creates the initial canvas
-function createCanvas() {
-  for(let i = 0; i <= 1495; i++){
-    const canvas_sect = document.querySelector('.canvas');
+function createCanvas(user_value) {
+  const canvas = document.querySelector('.canvas');
+  canvas.style.gridTemplateColumns = `repeat(${user_value} , 1fr)`;
+  canvas.style.gridTemplateRows = `repeat(${user_value} , 1fr)`;
+  for(let i = 1; i <= (user_value ** 2); i++){
     const canvas_units = document.createElement('div');
     canvas_units.classList.add('canvas-units');
-    canvas_units.classList.add('canvas-units-medium');
-    canvas_sect.appendChild(canvas_units); 
+    canvas.appendChild(canvas_units); 
   }
 }
 
-//BUTTON: This piece of code creates a canvas made out of small size divs and resets color to black.  
-const den_small = document.querySelector('.den-small');
-den_small.addEventListener('click', () => {
-  const canvas_sect = document.querySelector('.canvas');
-  while (canvas_sect.hasChildNodes()){
-    canvas_sect.removeChild(canvas_sect.firstChild);
-  }
-  for(let i = 0; i <= 3365; i++){
-    const canvas_sect = document.querySelector('.canvas');
-    const canvas_units = document.createElement('div');
-    canvas_units.classList.add('canvas-units');
-    canvas_units.classList.add('canvas-units-small');
-    canvas_sect.appendChild(canvas_units); 
-  }
-  const value = document.querySelector('input');
-  value.value = '#000000';
-  tone = '#000000';
-  paint();
-  clearInterval(multi_interval);
-}
-);
-
-//BUTTON: This piece of code creates a canvas made out of medium size divs and resets color to black.  
-const den_medium = document.querySelector('.den-medium');
-den_medium.addEventListener('click', () => {
-  const canvas_sect = document.querySelector('.canvas');
-  while (canvas_sect.hasChildNodes()){
-    canvas_sect.removeChild(canvas_sect.firstChild);
-  }
-  for(let i = 0; i <= 1495; i++){
-    const canvas_sect = document.querySelector('.canvas');
-    const canvas_units = document.createElement('div');
-    canvas_units.classList.add('canvas-units');
-    canvas_units.classList.add('canvas-units-medium');
-    canvas_sect.appendChild(canvas_units); 
-  }
-  const value = document.querySelector('input');
-  value.value = '#000000';
-  tone = '#000000';
-  paint();
-  clearInterval(multi_interval);
-}
-);
-
-//BUTTON: This piece of code creates a canvas made out of large size divs and resets color to black.  
-const den_large = document.querySelector('.den-large');
-den_large.addEventListener('click', () => {
-  const canvas_sect = document.querySelector('.canvas');
-  while (canvas_sect.hasChildNodes()){
-    canvas_sect.removeChild(canvas_sect.firstChild);
-  }
-  for(let i = 0; i <= 373; i++){
-    const canvas_sect = document.querySelector('.canvas');
-    const canvas_units = document.createElement('div');
-    canvas_units.classList.add('canvas-units');
-    canvas_units.classList.add('canvas-units-large');
-    canvas_sect.appendChild(canvas_units); 
-  }
-  const value = document.querySelector('input');
-  value.value = '#000000';
-  tone = '#000000';
-  paint();
-  clearInterval(multi_interval);
-}
-);
 
 //INPUT: This function allows the user to choose a color
 function chooseColor(){
   const color_input = document.querySelector('#color-picker');
   color_input.addEventListener('input', () => {
     tone = color_input.value;
-    clearInterval(multi_interval);
   })
 };
 
 //This piece of code allows the paint function to detect the mouse event
-let mouseDown = false
+let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
@@ -97,6 +53,7 @@ document.body.onmouseup = () => (mouseDown = false);
 //This function allows the user to 'paint' by changing the divs' background color to the desired color
 function paint(){
   const canvas_painted_units = document.querySelectorAll('.canvas-units');
+  console.log(canvas_painted_units);
   canvas_painted_units.forEach(unit => unit.addEventListener('mouseover', function (e) {
     if(e.type === 'mouseover' && !mouseDown){
       return
@@ -116,25 +73,50 @@ function paint(){
 //BUTTON: This function allows the user to 'erase' by changing the divs' background color to white
 function erase() {
 const erase_btn = document.querySelector('#erase');
+const color_input = document.querySelector('#color-picker');
 erase_btn.addEventListener('click', () => {
-  tone = '#FFFFFF';
-  clearInterval(multi_interval);
-})
-};
+  if(erase_btn.value === 'OFF'){
+    erase_btn.value = 'ON';
+    console.log(erase_btn.value);
+    tone = '#FFFFFF';
+    erase_btn.style.backgroundColor = 'white';
+    erase_btn.style.borderColor = color_input.value;
+  } else { 
+      erase_btn.value = 'OFF';
+      console.log(erase_btn.value);
+      tone = color_input.value;
+      erase_btn.style.backgroundColor = '#F6F1F4';
+      erase_btn.style.borderColor = '#EBF5F0';
+    }})
+clearInterval(multi_interval);
+}
 
 //BUTTON: This function allows the user to 'paint' by changing the divs' background color randomly in intervals
 function multicolor() {
   const multicolor_btn = document.querySelector('#multicolor');
+  const color_input = document.querySelector('#color-picker');
   multicolor_btn.addEventListener('click', () => {
-    multi_interval = setInterval(() => {
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    tone = '#' + randomColor;
-    }, 50);
-  })
-  };
+    if(multicolor_btn.value === 'OFF'){
+      multicolor_btn.value = 'ON';
+      console.log(multicolor_btn.value);
+      multi_interval = setInterval(() => {
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        tone = '#' + randomColor;
+        }, 50);
+      multicolor_btn.style.backgroundColor = '#FDA4BA';
+      multicolor_btn.style.borderColor = '#FDA4BA';
+    } else {
+      multicolor_btn.value = 'OFF';
+      console.log(multicolor_btn.value);
+      clearInterval(multi_interval);
+      tone = color_input.value;
+      multicolor_btn.style.backgroundColor = '#F6F1F4';
+      multicolor_btn.style.borderColor = '#EBF5F0';
+    }})
+  }
 
-createCanvas();
+  
+
 multicolor();
 erase();
-paint();
 chooseColor();
